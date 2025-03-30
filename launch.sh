0722e7795759feb0b4d3831845414492de370215
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2025-03-30 18:42:47 -0400

    feat: list out emulators

---
+++ launch.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+PAK_DIR="$(dirname "$0")"
+PAK_NAME="$(basename "$PAK_DIR")"
+PAK_NAME="${PAK_NAME%.*}"
+[ -f "$USERDATA_PATH/$PAK_NAME/debug" ] && set -x
+
+rm -f "$LOGS_PATH/$PAK_NAME.txt"
+exec >>"$LOGS_PATH/$PAK_NAME.txt"
+exec 2>&1
+
+echo "$0" "$@"
+cd "$PAK_DIR" || exit 1
+mkdir -p "$USERDATA_PATH/$PAK_NAME"
+
+architecture=arm
+if uname -m | grep -q '64'; then
+    architecture=arm64
+fi
+
+export PATH="$PAK_DIR/bin/$architecture:$PAK_DIR/bin/$PLATFORM:$PAK_DIR/bin:$PATH"
+
+show_message() {
+    message="$1"
+    seconds="$2"
+
+    if [ -z "$seconds" ]; then
+        seconds="forever"
+    fi
+
+    killall minui-presenter >/dev/null 2>&1 || true
+    echo "$message" 1>&2
+    if [ "$seconds" = "forever" ]; then
+        minui-presenter --message "$message" --timeout -1 &
+    else
+        minui-presenter --message "$message" --timeout "$seconds"
+    fi
+}
+
+cleanup() {
+    rm -f /tmp/stay_awake
+    rm -f /tmp/emus
+    rm -f /tmp/emus.list
+    rm -f /tmp/emus.json
+    rm -f /tmp/minui-output
+    killall minui-presenter >/dev/null 2>&1 || true
+}
+
+main() {
+    echo "1" >/tmp/stay_awake
+    trap "cleanup" EXIT INT TERM HUP QUIT
+
+    if [ "$PLATFORM" = "tg3040" ] && [ -z "$DEVICE" ]; then
+        export DEVICE="brick"
+        export PLATFORM="tg5040"
+    fi
+
+    allowed_platforms="tg5040 rg35xxplus"
+    if ! echo "$allowed_platforms" | grep -q "$PLATFORM"; then
+        show_message "$PLATFORM is not a supported platform" 2
+        return 1
+    fi
+
+    if ! command -v minui-presenter >/dev/null 2>&1; then
+        echo "minui-presenter not found" >&2
+        return 1
+    fi
+
+    chmod +x "$PAK_DIR/bin/$architecture/jq"
+    chmod +x "$PAK_DIR/bin/$PLATFORM/minui-presenter"
+
+    # get list of roms
+    find "$SDCARD_PATH/Roms" -maxdepth 1 -type d | sort >/tmp/emus
+    sed -i '/^[.]/d; /^APPS/d' /tmp/emus
+
+    touch /tmp/emus.list
+    while read -r file; do
+        basename "$file" >>/tmp/emus.list
+    done </tmp/emus
+
+    killall minui-presenter >/dev/null 2>&1 || true
+    minui-list --file "/tmp/emus.list" --cancel-text "EXIT" --title "Emulator Selection" --write-location /tmp/minui-output --write-value state
+}
+
+main "$@"
